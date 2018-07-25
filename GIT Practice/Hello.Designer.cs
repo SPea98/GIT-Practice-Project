50772a2837696d084af62f1a6ec4ea78f2f0fd29
photo <photo@DESKTOP-J5KG3UF>
2018-07-25 12:34:07 -0700

    Add project files.

---
+++ GIT Practice/Hello.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace GIT_Practice
+{
+    partial class Hello
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowMore = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnShowMore
+            // 
+            this.btnShowMore.Location = new System.Drawing.Point(129, 12);
+            this.btnShowMore.Name = "btnShowMore";
+            this.btnShowMore.Size = new System.Drawing.Size(75, 23);
+            this.btnShowMore.TabIndex = 0;
+            this.btnShowMore.Text = "Show More";
+            this.btnShowMore.UseVisualStyleBackColor = true;
+            this.btnShowMore.Click += new System.EventHandler(this.btnShowMore_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "No Resource Data Exist. Add to the data table(s)";
+            this.textBox1.Visible = false;
+            // 
+            // Hello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 284);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnShowMore);
+            this.Name = "Hello";
+            this.Text = "GIT Tutorial WIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowMore;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GIT Practice/Hello.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIT_Practice
+{
+    public partial class Hello : Form
+    {
+        public Hello()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowMore_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Visible = true;
+        }
+    }
+}
